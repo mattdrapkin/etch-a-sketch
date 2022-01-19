bfc18d633ba418193e0d87b54c657549083e1bdb
mattdrapkin <matt@drapkin.org>
2022-01-19 11:34:22 -0500

    Added color functionality

--- eas.js
+++ eas.js
@@ -2,6 +2,7 @@ const bigContainer = document.querySelector(".big-container");
 const containerSize = 30;
 const defGridSize = 64;
 let divSize = containerSize / defGridSize;
+let isColorMode = false;
 
 function makeGrid(gridSize) {
     for(let i = 0; i < gridSize; i++) {
@@ -12,7 +13,13 @@ function makeGrid(gridSize) {
             d.style.height = `${divSize}em`;
             bigContainer.appendChild(d);
             d.addEventListener("mouseover", () => {
-                d.style.backgroundColor = "black";
+                if(isColorMode) {
+                    let randRed = Math.random() * 255;
+                    let randGreen = Math.random() * 255;
+                    let randBlue = Math.random() * 255;
+                    d.style.backgroundColor = "rgb(" + randRed + "," +  randGreen + "," + randBlue + ")";
+                }
+                else d.style.backgroundColor = "black";
             })
         }
     }
@@ -28,6 +35,18 @@ reset.addEventListener("click", () => {
         newGridSize = parseInt(prompt("Number of squares per side of grid: (Defualt is 64, Maximum is 100)"))}
     while (newGridSize > 100);
     makeGrid(newGridSize);
+})
+
+const clrBtn = document.getElementById("color-btn");
+clrBtn.addEventListener("click", () => {
+    if(isColorMode) {
+        isColorMode = false;
+        clrBtn.innerHTML = "Color Mode"
+    }
+    else {
+        isColorMode = true;
+        clrBtn.innerHTML = "B&W Mode";
+    }
 })
 
 makeGrid(defGridSize);
